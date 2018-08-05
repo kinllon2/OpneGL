97c5d51d17a3ddecf24dff76efd8d9d63567d4f2
WangChengtao <821997225@qq.com>
2018-08-05 15:59:27 +0800

    [*]create a window

--- Leaen_OpenGL/Leaen_OpenGL/Hellow_Window.cpp
+++ Leaen_OpenGL/Leaen_OpenGL/Hellow_Window.cpp
@@ -1,45 +1,80 @@
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
+//GLAD是用来管理OpenGL的函数指针的
+#include<iostream>
+
+using namespace std;
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow *window);
+
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
 
 int main(void)
 {
-	GLFWwindow* window;
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR,3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR,3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	/* Initialize the library */
-	if (!glfwInit())
-		return -1;
-
-
+#ifdef __APPLE__
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
 	
 
-	/* Create a windowed mode window and its OpenGL context */
-	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-	if (!window)
+	//
+	GLFWwindow* window = glfwCreateWindow(800,600,"LearnOpenGL",NULL,NULL);
+	if (window == NULL)
 	{
+		cout << "Failed to create GLFW window" << endl;
 		glfwTerminate();
 		return -1;
 	}
-
-	/* Make the window's context current */
+	//Making the OpenGL context current
 	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-	/* Loop until the user closes the window */
+	//我们给GLAD传入了用来加载系统相关的OpenGL函数指针地址的函数。GLFW给我们的是glfwGetProcAddress，它根据我们编译的系统定义了正确的函数。
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+
+	// render loop
+	//主循环
 	while (!glfwWindowShouldClose(window))
 	{
+		// input
+		// -----
+		processInput(window);
 
-
-
-
-
-		/* Render here */
+		// render
+		// ------
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
-
-		/* Swap front and back buffers */
+		
+		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+		// -------------------------------------------------------------------------------
 		glfwSwapBuffers(window);
-
-		/* Poll for and process events */
 		glfwPollEvents();
+		//double time = glfwGetTime();
 	}
 
 	glfwTerminate();
 	return 0;
+}
+
+
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow *window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
 }
