f3ab83923cb5e18f52137a5b59b0a8d8bad41213
WangChengtao <821997225@qq.com>
2018-08-06 14:59:37 +0800

    [*]  add  a vao to control the vbo

--- Leaen_OpenGL/Leaen_OpenGL/Hellow_Window.cpp
+++ Leaen_OpenGL/Leaen_OpenGL/Hellow_Window.cpp
@@ -117,9 +117,21 @@ int main(void)
 
 	unsigned int VBO, VAO, BEO;
 
+	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
+
+	glBindVertexArray(VAO);
+
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+	glEnableVertexAttribArray(0);
+	
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	//UNBIND VAO
+	glBindVertexArray(0);
 
 
 
@@ -136,13 +148,20 @@ int main(void)
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 		
+		// Draw our first triangle
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glBindVertexArray(0);
+
 		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
 		// -------------------------------------------------------------------------------
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 		//double time = glfwGetTime();
 	}
-
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
 	glfwTerminate();
 	return 0;
 }
