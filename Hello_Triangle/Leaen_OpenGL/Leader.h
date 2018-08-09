2f10df165e248a21cacf2fed1716719259b0a886
WangChengtao <821997225@qq.com>
2018-08-09 21:23:22 +0800

    []ts

--- Hello_Triangle/Leaen_OpenGL/Leader.h
+++ Hello_Triangle/Leaen_OpenGL/Leader.h
@@ -1,9 +1,9 @@
 #pragma once
 //定點信息和index信息
-//#define VEC_INDEX_OPEN
+#define VEC_INDEX_OPEN
 //添加更多顶点到数据中，使用glDrawArrays，尝试绘制两个彼此相连的三角形
 //#define VEC_INONE_ARRAY_OPEN
 //创建相同的两个三角形，但对它们的数据使用不同的VAO和VBO：
  //#define DIFF_VAO_VBO_OPEN
 //创建两个着色器程序，第二个程序使用一个不同的片段着色器，输出黄色；再次绘制这两个三角形，让其中一个输出为黄色
-#define TWO_FP
+//#define TWO_FP
